57428e7a73d76c8edbfa74e55ec453f0257b5d1a
unknown <camilomacoris@hotmail.com>
2022-07-12 15:48:46 -0300

    refatoração de todo javascript do codigo, resolvendo problemas de imutabilidade

--- app.js
+++ app.js
@@ -1,85 +1,106 @@
-const currencyOne = document.querySelector('[data-js="currency-one"]')
-const currencyTwo = document.querySelector('[data-js="currency-two"]')
+const currencyOneEl = document.querySelector('[data-js="currency-one"]')
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
 const inputCurrency = document.querySelector('[data-js="currency-one-times"]')
 const convertedValue = document.querySelector('[data-js="converted-value"]')
 const presionValue = document.querySelector('[data-js="conversion-precision"]')
 
-const APIKey = `77eba15c76a727df52ae4b91`
-let currency = `USD`
-let url = `https://v6.exchangerate-api.com/v6/${APIKey}/latest/${currency}`
+const APIKey = `01ec3b20673ff8304a2bdbed`
 
-const fetchApi = async url => {
-  try{
-    const response = await fetch(url)
+let getUrl = currency => `https://v6.exchangerate-api.com/v6/${APIKey}/latest/${currency}`
+
+const fetchApi = async currency => {
+  try {
+    const response = await fetch(getUrl(currency))
 
     if (!response.ok) {
       throw new Error('Não foi possível carregar os dados das moedas!')
     }
-  
+
     return response.json()
-  }catch({name, message}) {
+  } catch ({ name, message }) {
     alert(`${name}: ${message}`)
   }
 }
 
-let getCurrencyOne, getCurrencyTwo, selectedCurrencyOne, selectedCurrencyTwo
-
-const generateCurrancyOptions = (arrOfCurrencys, pattern1 = 'USD', patter2 = 'BRL') => 
-  arrOfCurrencys.map((currency) => {   
-    if (currency.includes(pattern1)) {
-      selectedCurrencyOne = `<option selected value="${currency}">${currency[0]}</option>`
-    } else if (currency.includes(patter2)) {
-      selectedCurrencyTwo = `<option selected value="${currency}">${currency[0]}</option>`
-    } 
-    return `<option value="${currency}">${currency[0]}</option>`
-  })
-
-const populateSelect = (arrOfCurrencyOptions, pattern1, pattern2) => 
-  arrOfCurrencyOptions.forEach((optCurrency) => {
-    currencyOne.innerHTML += optCurrency.includes(pattern1) ? selectedCurrencyOne : optCurrency
-    currencyTwo.innerHTML += optCurrency.includes(pattern2) ? selectedCurrencyTwo : optCurrency
-  })
-
-const showPrecisionValue = () => 
-  presionValue.textContent = `Valor Unitário: ${getCurrencyOne[1]} ${getCurrencyOne[0]} = ${getCurrencyTwo[1]} ${getCurrencyTwo[0]}`
-
-const getSelectedCurrencys = (arrOfCurrencys, pattern1, pattern2) => {
-  getCurrencyOne = arrOfCurrencys.find(element => element.includes(pattern1))
-  getCurrencyTwo = arrOfCurrencys.find(element => element.includes(pattern2))
+const getPrecisionValues = (currencyOneText, currencyTwoText, currencyTwoValue) => {
+  presionValue.textContent = `Valor Unitário: 1 ${currencyOneText} = ${currencyTwoValue} ${currencyTwoText}`
 }
 
-const getAllCurrencys = async (pattern1 = 'USD', pattern2 = 'BRL') => {
-  const {conversion_rates} = await fetchApi(url)
-  const allCurrencys = Object.entries(conversion_rates)
-
-  getSelectedCurrencys(allCurrencys, pattern1, pattern2)
-  const currencys = generateCurrancyOptions(allCurrencys, pattern1, pattern2)
-  populateSelect(currencys, pattern1, pattern2)
-  showPrecisionValue()  
+const getAllCurrencys = async (currency = 'USD') => {
+  const { conversion_rates } = await fetchApi(currency)
+  return conversion_rates
 }
 
-const changeSelectValueOne =  async event => {
-  const selectCurrencyOneValue = event.target.value.split(',')[0]
-  url = `https://v6.exchangerate-api.com/v6/${APIKey}/latest/${selectCurrencyOneValue}`
-  await getAllCurrencys(selectCurrencyOneValue, currencyTwo.value.split(',')[0])
+const generateCurrancyOptions = async (allCurrencies, currency1 = 'USD', currency2 = 'BRL') => {
+  const currencyValue = { ...await allCurrencies }
+  const currencies = Object.keys(currencyValue)
+  getPrecisionValues(currency1, currency2, currencyValue[currency2])
+
+  const optionsCurrenies = currencies.map((currency) => {
+    if (currency === currency1 || currency === currency2) {
+      return `<option data-currency="${currency}" selected value="${currencyValue[currency]}">${currency}</option>`
+    }
+    return `<option data-currency="${currency}" value="${currencyValue[currency]}">${currency}</option>`
+  })
+
+  return optionsCurrenies
+}
+
+const populateSelect = async (arrOfCurrencyOptions, currency1 = 'USD', currency2 = 'BRL') => {
+  const arrayCurrentOpt = await arrOfCurrencyOptions
+  
+  arrayCurrentOpt.forEach((optCurrency) => {
+    if (optCurrency.includes(currency1)) {
+      currencyOneEl.innerHTML += optCurrency
+      return
+    }
+
+    if (optCurrency.includes(currency2)) {
+      currencyTwoEl.innerHTML += optCurrency
+      return
+    }
+
+    currencyOneEl.innerHTML += optCurrency
+    currencyTwoEl.innerHTML += optCurrency
+  })
+}
+
+const calculateChangedInput = currency =>
+  convertedValue.textContent = (inputCurrency.value * currency).toFixed(2)
+
+const changeSelectValueOne = async event => {
+  const currencyOneText = event.target.options[event.target.selectedIndex].textContent
+  const currencyOneValue = event.target.options[event.target.selectedIndex].value
+  const currencyTwoText = currencyTwoEl.options[currencyTwoEl.selectedIndex].textContent
+  const currencyTwoValue = currencyTwoEl.options[currencyTwoEl.selectedIndex].value
+  
+  calculateChangedInput(currencyOneValue)
+  getAllCurrencys(currencyOneText)
+  getPrecisionValues(currencyOneText, currencyTwoText, currencyTwoValue)
 }
 
 const changeSelectValueTwo = async event => {
-  const selectCurrencyTwoValue = event.target.value.split(',')[0]
-  await getAllCurrencys(currencyOne.value.split(',')[0], selectCurrencyTwoValue)
+  const currencyTwoValue = event.target.options[event.target.selectedIndex].value
+  const currencyTwoText = event.target.options[event.target.selectedIndex].textContent
+  const currencyOneElText = currencyOneEl.options[currencyOneEl.selectedIndex].textContent
+  
+  calculateChangedInput(currencyTwoValue)
+  getPrecisionValues(currencyOneElText, currencyTwoText, currencyTwoValue)
 }
 
-const calculateCurrecy =  event => {
+const calculateCurrecy = event => {
   const inputValue = event.target.valueAsNumber
-  const calculate = inputValue * getCurrencyTwo[1]
+  const currencyTwoElValue = currencyTwoEl.options[currencyTwoEl.selectedIndex].value
+  const calculate = inputValue * currencyTwoElValue
+  
   if (!isNaN(calculate)) {
     convertedValue.textContent = calculate.toFixed(2)
-  }  
+  }
 }
 
-currencyOne.addEventListener('input',changeSelectValueOne)
-currencyTwo.addEventListener('input', changeSelectValueTwo)
-inputCurrency.addEventListener('input',calculateCurrecy)
+currencyOneEl.addEventListener('input', changeSelectValueOne)
+currencyTwoEl.addEventListener('input', changeSelectValueTwo)
+inputCurrency.addEventListener('input', calculateCurrecy)
 
-getAllCurrencys() 
-
+const optionsCurrenies = generateCurrancyOptions(getAllCurrencys())
+populateSelect(optionsCurrenies)
